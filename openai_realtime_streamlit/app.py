035e16c98c36cef98c30fb8ee7987e06560caae9
Michelangelo D'Agostino <mdagost@gmail.com>
2024-10-06 20:55:15 -0500

    A few cleanups and docstrings.

--- openai_realtime_streamlit/app.py
+++ openai_realtime_streamlit/app.py
@@ -17,12 +17,16 @@ st.set_page_config(layout="wide")
 if "logs" not in st.session_state:
     st.session_state.logs = []
 
-def get_event_loop():
-    return asyncio.new_event_loop()
-
 
 @st.cache_resource(show_spinner=False)
 def create_loop():
+    """
+    Creates an event loop we can globally cache and then run in a
+    separate thread.  Many, many thanks to
+    https://handmadesoftware.medium.com/streamlit-asyncio-and-mongodb-f85f77aea825
+    for this tip.  NOTE: globally cached resources are shared across all users
+    and sessions, so this is only okay for a local R&D app like this.
+    """
     loop = asyncio.new_event_loop()
     thread = Thread(target=loop.run_forever)
     thread.start()
@@ -30,16 +34,23 @@ def create_loop():
 
 st.session_state.event_loop, worker_thread = create_loop()
 
+
 def run_async(coroutine):
+    """
+    Helper for running an async function in the globally cached event loop we
+    just created.
+    """
     return run_coroutine_threadsafe(coroutine, st.session_state.event_loop).result()
 
 
 @st.cache_resource(show_spinner=False)
 def setup_client():
+    """
+    Globally cached SimpleRealtime client.
+    """
     if client := st.session_state.get("client"):
         return client
     return SimpleRealtime(event_loop=st.session_state.event_loop, debug=True)
-
 
 st.session_state.client = setup_client()
 
@@ -47,6 +58,7 @@ st.session_state.client = setup_client()
 @st.fragment(run_every=1)
 def logs_text_area():
     logs = st.session_state.client.logs
+
     if st.session_state.show_full_events:
         for _, _, log in logs:
             st.json(log, expanded=False)
@@ -60,6 +72,9 @@ def logs_text_area():
 
 
 def st_app():
+    """
+    Our main streamlit app function.
+    """
     st.markdown(HIDE_STREAMLIT_RUNNING_MAN_SCRIPT, unsafe_allow_html=True)
 
     main_tab, docs_tab = st.tabs(["Console", "Docs"])
@@ -79,16 +94,19 @@ def st_app():
                     except Exception as e:
                         st.error(f"Error connecting to OpenAI Realtime API: {str(e)}")
 
-        st.session_state.show_full_events = st.checkbox("Show Full Events", value=False)
+        st.session_state.show_full_events = st.checkbox("Show Full Event Payloads", value=False)
         with st.container(height=300, key="logs_container"):
             logs_text_area()
 
         _ = st.text_area("Enter your message:", key = "input_text_area", height=200)
         def clear_input_cb():
+            """
+            Callback that will clear our message input box after the user
+            clicks the send button.
+            """
             st.session_state.last_input = st.session_state.input_text_area
             st.session_state.input_text_area = ""
 
-        # Send button
         if st.button("Send", on_click=clear_input_cb, type="primary"):
             if st.session_state.get("last_input"):
                 try:
